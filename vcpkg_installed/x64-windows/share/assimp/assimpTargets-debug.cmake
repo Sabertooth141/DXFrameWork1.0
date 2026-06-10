aa5b438b803c9ceee0525ca551ef6fba7d723584
Sabertooth141 <44132181+Sabertooth141@users.noreply.github.com>
2026-06-10 23:57:34 +0900

    update

--- vcpkg_installed/x64-windows/share/assimp/assimpTargets-debug.cmake
+++ vcpkg_installed/x64-windows/share/assimp/assimpTargets-debug.cmake
@@ -8,13 +8,13 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "assimp::assimp" for configuration "Debug"
 set_property(TARGET assimp::assimp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(assimp::assimp PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/assimp-vc143-mtd.lib"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/assimp-vc145-mtd.lib"
   IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "poly2tri::poly2tri;unofficial::minizip::minizip;zip::zip"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/assimp-vc143-mtd.dll"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/assimp-vc145-mtd.dll"
   )
 
 list(APPEND _cmake_import_check_targets assimp::assimp )
-list(APPEND _cmake_import_check_files_for_assimp::assimp "${_IMPORT_PREFIX}/debug/lib/assimp-vc143-mtd.lib" "${_IMPORT_PREFIX}/debug/bin/assimp-vc143-mtd.dll" )
+list(APPEND _cmake_import_check_files_for_assimp::assimp "${_IMPORT_PREFIX}/debug/lib/assimp-vc145-mtd.lib" "${_IMPORT_PREFIX}/debug/bin/assimp-vc145-mtd.dll" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
